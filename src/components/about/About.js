ff188e6b1ff72f5d70863722324c2584c9684cc5
willtrotman <willtrotmanmusic@gmail.com>
2022-05-17 14:23:51 -0500

    fixed typo

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,9 +7,9 @@ export function About({ isToggled }) {
         Front End Developer
       </h2>
       <p className={classes.about}>
-        Welcome to my portfolio! I'm a musician gone web developer, primarily
-        working in React. I enjoy writing clean, reusable code and designing
-        smooth user interfaces.
+        Welcome to my portfolio! I'm a musician gone web developer, who
+        primarily works in React. I enjoy writing clean, reusable code and
+        designing smooth user interfaces.
       </p>
       {/* <div className={classes.email}>willtrotman336@gmail.com</div> */}
     </div>
